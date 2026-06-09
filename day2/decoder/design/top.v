4a00bf1aa2feb4358f297589d490528302c52f73
Shreyakrishnan3 <krishshreya0122@gmail.com>
2026-06-09 21:30:21 +0530

    Update top.v

--- day2/decoder/design/top.v
+++ day2/decoder/design/top.v
@@ -1,2 +1,38 @@
-module decoder();
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 21:27:49
+// Design Name: 
+// Module Name: decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module decoder(
+input [1:0] b,
+output reg [3:0] D
+);
+always @(*) begin
+case (b)
+2'b00: D = 4'b0001;
+2'b01: D = 4'b0010;
+2'b10: D = 4'b0100;
+2'b11: D = 4'b1000;
+default: D = 4'b0000;
+endcase
+end
 endmodule
+
