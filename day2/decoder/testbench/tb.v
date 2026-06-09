610a9ec705b48cc7eca8bff5a8109ce98fa29915
Shreyakrishnan3 <krishshreya0122@gmail.com>
2026-06-09 21:36:19 +0530

    Update tb.v

--- day2/decoder/testbench/tb.v
+++ day2/decoder/testbench/tb.v
@@ -1,1 +1,41 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 21:33:37
+// Design Name: 
+// Module Name: decoder_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+
+module decoder_tb();
+reg [1:0] b_tb;
+wire [3:0] D_tb;
+decoder dut(
+.b(b_tb),
+.D(D_tb)
+);
+initial begin
+b_tb = 2'b00;
+#10;
+b_tb = 2'b01;
+#10;
+b_tb = 2'b10;
+#10;
+b_tb = 2'b11;
+#10;
+end
+endmodule
